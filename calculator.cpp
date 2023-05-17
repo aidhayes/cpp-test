ce6347cde463d5579238c83b9e7ed7eef1f6be3d
aidhayes <aid.hayes01@gmail.com>
2023-05-17 18:12:58 -0400

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, y;
+    int sum;
+
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Enter another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Your sum is " << sum;
+}
