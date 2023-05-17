c8a6ebabd19bb18d00f70f8902eaa436d3a8c710
aidhayes <aid.hayes01@gmail.com>
2023-05-17 17:54:13 -0400

    Variable and addition tests

--- test.cpp
+++ test.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!";
+    // Basic print text
+    cout << "Hello world!" << endl;
+
+    // Variable
+    int num = 17;
+    cout << num << endl;
+    cout << "The number is " << num << endl;
+
+    // Addition
+    int x = 15;
+    int y = 12;
+    int sum = x + y;
+    cout << sum << endl;
+
+
+
     return 0;
 }
